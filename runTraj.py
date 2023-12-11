fae3b2e0c5f80d9adb756a6352c431f7bcb759f8
ryan-aday <33794562+ryan-aday@users.noreply.github.com>
2023-12-11 02:24:29 -0500

    Add files via upload
    Fixed bugs, fixed global ECEF coordinate outputs through a flat-earth solution.

--- runTraj.py
+++ runTraj.py
@@ -2,14 +2,15 @@
 runTraj.py
 Written by Ryan Aday
 Copyright @2023
-V2: 12/06/2023- Added Hughes transform matrix capability, revised relative to ECEF algorithm
+V3: 12/11/2023- Added flat earth/LLA solver, commented out the Hughes matrix solver
 
-The Hughes transform matrix performs a coordinate transformation by rotating and translating the emplacement coordinate system into that of the globe model. 
-This approach is correct.
+The current algorithm first converts the emplacement location from geodetic/LLA into flat-earth XYZ coordinates. 
+The XYZ trajectory data is then added to the flat-earth XYZ coordinates, after which the sums are converted
+into geodetic/LLA coordinates. These are converted to geocentric coordinates (WGS84 currently). 
 
-The existing relative to ECEF algorithm converts the relative Cartesian coordinates into the relative latitude/longitude angles & altitude.
-This is then added to the radar latitude/longitude angles and the altitude to get the correct ECEF coordinates of a trajectory.
-This approach is wrong.
+The Hughes transform matrix performs a coordinate transformation by rotating and translating the emplacement
+coordinate system into that of the globe model. 
+This approach is incorrect, as it assumes incorrectly that the earth's surface is flat as it plots trajectories.
 '''
 
 # Import libraries
@@ -17,7 +18,7 @@ import math, numpy
 
 '''
 #############################################################################################
-User Constant Inputs
+User Constant Inumpyuts
 #############################################################################################
 '''
 
@@ -37,7 +38,7 @@ distance_from_radar = 80000.0 # meters
 search_sector_angle = 120.0 # degrees
 bearing_N = 0.0 # 6133.3 # mills (Note: 1 degree = 160/9 milla)
 speed = 210.0 # m/s
-altitude = 40000.0 # meters
+altitude = 80000.0 # meters
 start_time = 0.0 # seconds
 end_time = 5000.0 # seconds
 
@@ -53,8 +54,8 @@ yaw = 0.0 # RCS degrees
 Conversion Functions ***DO NOT MODIFY
 #############################################################################################
 '''
-
-# Uncomment for Hughes transform
+'''
+### Hughes transform functions ###
 def Rz(z_angle):
     return numpy.array([\
     [math.cos(z_angle), math.sin(z_angle), 0],\
@@ -63,14 +64,14 @@ def Rz(z_angle):
 
 def Ry(y_angle):
     return numpy.array([\
-    [math.cos(y_angle, 0, math.sin(y_angle)],\
+    [math.cos(y_angle), 0, math.sin(y_angle)],\
     [0, 1, 0],\
     [-math.sin(y_angle), 0, math.cos(y_angle)],])
     
 def Rx(x_angle):
     return numpy.array([\
     [1, 0, 0],\
-    [0, -math.cos(x_angle), -math.sin(z.angle)],\
+    [0, -math.cos(x_angle), -math.sin(x_angle)],\
     [0, math.sin(x_angle), math.cos(x_angle)],])
     
 def Rt(x_angle, y_angle, z_angle):
@@ -95,36 +96,65 @@ def H(Rt, x_ECEF, y_ECEF, z_ECEF):
     Hf[1][3] = y_ECEF
     Hf[2][3] = z_ECEF
     Hf[3][3] = 1
+    
+    return Hf
 
-def resM(H, x, y, z):
-    ref = numpy.array([[x], [y], [z], [1]])
+def resM(Ht, x, y, z):
+    ref = numpy.array([x, y, z, 1])
     # X, Y, Z should be the absolute ECEF coordinates
-    return H * ref
+    return numpy.dot(Ht, ref)
 
 def angle(v1, v2):
     # Assumes v1 and v2 are both 1x3 vectors, outputs in rads
     return numpy.arccos(numpy.dot(v1, v2)/(numpy.linalg.norm(v1) * numpy.linalg.norm(v2)))
+'''
 
 def geodetic_to_geocentric(ellps, lat, lon, h):
     a, rf = ellps
+    
+    b = a * (1 - 1/rf)
+    e = math.sqrt(1- (b**2/a**2))
+    
     lat_rad = math.radians(lat)
     lon_rad = math.radians(lon)
-    N = a / math.sqrt(1 - (1 - (1 - 1 / rf) ** 2) * (math.sin(lat_rad))**2)
+    
+    #N = a / math.sqrt(1 - (1 - (1 - 1 / rf) ** 2) * (math.sin(lat_rad))**2)
+    N = a / math.sqrt(1 - e**2*math.sin(lat_rad)**2)
+    
     X = ((N + h) * math.cos(lat_rad) * math.cos(lon_rad))
     Y = ((N + h) * math.cos(lat_rad) * math.sin(lon_rad))    
-    Z = ((1 - 1 / rf) ** 2 * N + h) * math.sin(lat_rad)
-    
+    #Z = ((1 - 1 / rf) ** 2 * N + h) * math.sin(lat_rad)
+    Z = ((1 - e) ** 2 * N + h) * math.sin(lat_rad)
+
     return X, Y, Z
     
-def relative_to_geodetic(ellps, x, y, z):
+def ECEF_flat_to_geodetic(ellps, x, y, z):
     R, rf = ellps
+    r = math.sqrt(x**2 + z**2)
     
-    lon = math.degrees(math.atan2(y, x))
-    p = math.sqrt(x**2 + y**2)
-    lat = math.degrees(math.atan2(p, z))
+    lat = math.degrees(math.pi/2 - r/R)
+    lon = math.degrees(math.acos(z/r) * sign(x))
+    #lon = math.degrees(math.atan2(y, x))
+    #lat = math.degrees(math.atan2(p, z))
     h = y
     
     return lat, lon, h
+    
+def geodetic_to_ECEF_flat(ellps, lat, lon, h):
+    R, rf = ellps
+    
+    lat_rad = math.radians(lat)
+    lon_rad = math.radians(lon)
+    
+    A = (math.pi / 2 - lat_rad) * R
+    x = A * math.sin(lon_rad)
+    y = h
+    z = A * math.cos(lon_rad)
+    return x, y, z
+    
+    
+def sign(x):
+    return 1.0 if x>= 0 else -1.0
     
 '''
 #############################################################################################
@@ -137,18 +167,28 @@ Functions
 wgs84 = (6378137, 298.257223563)
 
 # ECEF Reference Vectors
-X_ECEF_ref = [1, 0, 0]
-Y_ECEF_ref = [0, 1, 0]
-Z_ECEF_ref = [0, 0, 1]
+'''
+x_ECEF_ref = [1, 0, 0]
+y_ECEF_ref = [0, 1, 0]
+z_ECEF_ref = [0, 0, 1]
+'''
 
 # ECEF Conversion of emplacement location
-ECEF_X, ECEF_Y, ECEF_Z = geodetic_to_geocentric(wgs84, wgs84_lat, wgs84_lon, wgs_84_alt)
-radar_ECEF = [ECEF_X, ECEF_Y, ECEF_Z]
+x_radar_ECEF, y_radar_ECEF, z_radar_ECEF = geodetic_to_geocentric(wgs84, wgs84_lat, wgs84_lon, wgs84_alt)
+radar_ECEF = [x_radar_ECEF, y_radar_ECEF, z_radar_ECEF]
 
 # Derive H Transform Matrix
-x_angle = angle(radar_ECEF, X_ECEF_ref)
-y_angle = angle(radar_ECEF, Y_ECEF_ref)
-z_angle = angle(radar_ECEF, Z_ECEF_ref)
+'''
+x_angle = angle(radar_ECEF, x_ECEF_ref)
+y_angle = angle(radar_ECEF, y_ECEF_ref)
+z_angle = angle(radar_ECEF, z_ECEF_ref)
+
+R = Rt(x_angle, y_angle, z_angle)
+Ht = H(R, x_radar_ECEF, y_radar_ECEF, z_radar_ECEF)
+'''
+
+# Set up flat XYZ references for the flat globe solution
+x_flat_ref, y_flat_ref, z_flat_ref = geodetic_to_ECEF_flat(wgs84, wgs84_lat, wgs84_lon, wgs84_alt)
 
 # Set the time interval to be 0.1 seconds
 time_interval = 0.1 # seconds
@@ -189,26 +229,32 @@ for t in numpy.arange(start_time, end_time + time_interval, time_interval):
     x = distance_from_radar * math.sin(start_angle_offset + angular_velocity * t) # km
     y = altitude # km
     z = distance_from_radar * math.cos(start_angle_offset + angular_velocity * t) # km
-    
-    # LLA solution
-    '''
-    lat_sim, lon_sim, h_sim = relative_to_geodetic(wgs84, x, y, z)
-    x_ECEF, y_ECEF, z_ECEF = geodetic_to_geocentric(wgs84, wgs84_lat + lat_sim,\
-    wgs84_lon + lon_sim, wgs84_alt + h_sim)
-    '''
-    
+      
     # Hughes transform solution
-    sol = resM(H, x, y, z)
+    '''
+    #sol = resM(Ht, x_ECEF, y_ECEF, z_ECEF)
+    sol = resM(Ht, x, y, z)
+
     x_ECEF = sol[0]
     y_ECEF = sol[1]
     z_ECEF = sol[2]
-
+    '''
+    
     '''
     #############################################################################################
     End of user-defined X, Y, Z trajectory characteristics
     #############################################################################################
-    '''        
+    '''   
+
+    # Convert to ECEF using the flat LLA solution
+    x_flat = x + x_flat_ref
+    y_flat = y + y_flat_ref    
+    z_flat = z + z_flat_ref
+
+    lat, lon, h = ECEF_flat_to_geodetic(wgs84, x_flat, y_flat, z_flat)
+    x_ECEF, y_ECEF, z_ECEF = geodetic_to_geocentric(wgs84, lat, lon, h)
     
+    # Store data in arrays
     t_intervals.append(t) # seconds
     x_coordinates.append(x/1000) # km
     y_coordinates.append(y/1000) # km
@@ -224,13 +270,14 @@ for t in numpy.arange(start_time, end_time + time_interval, time_interval):
     
 # Create a list of tuples
 trajectory_data = [(t, x, y, z, xE, yE, zE, p, R, lR, hR, yw) for t, x, y, z, xE, yE, zE, p, R, lR, hR, yw in \
-x_ECEF_coordinates, y_ECEF_coordinates, z_ECEF_coordinates,\
-pitch_intervals, RCS_intervals, low_RCS_intervals, high_RCS_intervals, yaw_intervals)]
+zip(t_intervals, x_coordinates, y_coordinates, z_coordinates,\
+x_ECEF_coordinates, y_ECEF_coordinates, z_ECEF_coordinates, pitch_intervals,\
+RCS_intervals, low_RCS_intervals, high_RCS_intervals, yaw_intervals)]
 
 # Write the data to a .txt file
 with open(filename, 'w') as file:
     if (isGlobal):
-        print("\n Creating ECEF trajectory.")
+        print("\nCreating ECEF trajectory.")
         for data_point in trajectory_data:
             file.write("%s %s %s %s %s %s %s %s %s\n" % (format(data_point[0], '.1f'), format(data_point[4], '.6f'),\
             format(data_point[5], '.6f'), format(data_point[6], '.6f'), format(data_point[7], '.4f'), format(data_point[8], '.4f'),\
